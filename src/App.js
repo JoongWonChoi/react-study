96200f09de983c8a93329985374e392774a3ae2e
JoongWonChoi <qweasd1130@naver.com>
2022-06-29 15:40:08 +0900

    Create Repository

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import './App.css';
+import TOC from "./components/TOC.js";
+import Subject from "./components/Subject.js";
+import Content from "./components/Content.js";
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      mode : "read",
+      welcome : {title : "Welcome", desc : "welcome!!"},
+      subject : {title : "WEB", sub : "world wide web!"},
+      contents : [
+        {id:1, title:"HTML", desc:"HTML is HyperText. ."},
+        {id:2, title:"CSS", desc:"CSS is for design"},
+        {id:3, title:"JavaScript", desc:"Javascript is for interactive"}
+      ]
+    }
+  }
+  render() {
+    console.log("App render");
+    var _title, _desc = null;
+    if(this.state.mode === "welcome"){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+
+    }
+    else if(this.state.mode === "read"){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+    console.log('render',this);
+    return (
+      <div className="App">
+          {/* <Subject title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+          </Subject> */}
+          <header>
+            <h1><a href="/" onClick = {function(event){
+              console.log(event);
+              event.preventDefault();
+              this.setState({mode : "welcome"});
+            }.bind(this)}>{this.state.subject.title}</a></h1>
+            {this.state.subject.sub}
+          </header>
+          <TOC data={this.state.contents}></TOC>
+          <Content title={_title} desc={_desc}></Content>
+      </div>
+    );
+  }
+}
+
+export default App;
